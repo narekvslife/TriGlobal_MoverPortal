ed912a4d67e7ef9afebfc8c61984e90c3daab3dc
narekvslife <al_narek@mail.ru>
2019-05-07 13:46:36 +0300

    Initial

---
+++ TriGlobal_MoverPortal/TriGlobal_MoverPortal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriGlobal_MoverPortal
+//
+//  Created by narekvslife on 07/05/2019.
+//  Copyright © 2019 Narek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
