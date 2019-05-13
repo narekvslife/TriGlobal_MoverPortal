56f3bd06a24f9f8c43aae6174eb44cc65b126033
narekvslife <al_narek@mail.ru>
2019-05-13 10:14:58 +0300

    fix?

---
+++ TriGlobal_MoverPortal/TriGlobal_MoverPortal/Controllers/DetailedLeadViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailedLeadViewController.swift
+//  TriGlobal_MoverPortal
+//
+//  Created by narekvslife on 12/05/2019.
+//  Copyright © 2019 Narek. All rights reserved.
+//
+
+import UIKit
+
+class DetailedLeadViewController: UIViewController {
+
+    
+    //  TODO 55 56
+    private var leadInfo: Lead? =  nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TriGlobal_MoverPortal/TriGlobal_MoverPortal/Controllers/LeadsViewController.swift
+++ TriGlobal_MoverPortal/TriGlobal_MoverPortal/Controllers/LeadsViewController.swift
@@ -34,14 +34,27 @@ class LeadsViewController: UITableViewController {
        return cell
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if let id = segue.identifier{
+            switch id {
+            case "ShowDetailsSeague":
+                if let cell = sender as? LeadsTableViewCell,
+                    let indexPath = tableView.indexPath(for: cell),
+                    let sequedTo = segue.destination as? DetailedLeadViewController{
+                    // TODO 55:56
+                }
+                
+            default: break
+                
+            }
+        }
     }
-    */
-
+    
 }
